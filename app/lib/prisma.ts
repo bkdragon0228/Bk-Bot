5a5a4979fa234361fdd906c921fd8d70f15a520c
bkdragon0228 <77627957+bkdragon0228@users.noreply.github.com>
2024-12-02 21:32:06 +0900

    fix : fix prisma client genrate method

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,3 +1,13 @@
-import { PrismaClient } from "@prisma/client";
+// @ts-nocheck
+import { PrismaClient } from "../../prisma/generated/client";
 
-export const prisma = new PrismaClient();
+export let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient();
+} else {
+    if (!global.prisma) {
+        global.prisma = new PrismaClient();
+    }
+    prisma = global.prisma;
+}
